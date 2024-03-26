f25641a8301c1a485ab92b05616ab882485227f5
TurqaySafarov <161557826+TurqaySafarov@users.noreply.github.com>
2024-03-26 13:56:36 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+// Asagidakilari arasdirin: 
+// concat- birləşdirmədir
+// repeat- təkrarlananı kopyalayır
+// match-
+// test
+
+
+// 1.İstifadəçinin daxil etdiyi adın neçə simvoldan ibarət olduğunu tapın.
+// let name = prompt("İstifadəçi adını daxil edin: ");
+// console.log(name.length);
+
+// 2.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvol varmı ?.
+// let word = prompt("Söz daxil edin: ");
+// let symbol = prompt("Simvol daxil edin: "); 
+// console.log(word.indexOf(symbol));
+
+// 3.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla başlayırmı ?.
+// let word = prompt("Söz daxil edin: ");
+// let symbol = prompt("Simvol daxil edin: ");
+// console.log(word.startsWith(symbol));
+
+// 4.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla bitirmi ?.
+// let word = prompt("Söz daxil edin: ");
+// let symbol = prompt("Simvol daxil edin: ");
+// console.log(word.endsWith(symbol));
+
+// 5.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvolun index`ini tapın.
+// let word = prompt("Söz daxil edin: ");
+// let symbol = prompt("Simvol daxil edin:");
+// symbol=indexOf(symbol)
+// console.log(word.charAt(symbol));
+
+// 6.İstifadəçinin daxil etdiyi sözün böyük hərf olub olmadığını tapın.
+// let word = prompt("Söz daxil edin: ");
+// let symbol = prompt("Simvol daxil edin: ");
+// if (word.toUpperCase()==word){
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
+
+
+
+
+
+
+
